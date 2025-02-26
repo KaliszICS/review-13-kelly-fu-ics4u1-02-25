79d1174c0cf8345564848f462e008c68b5d87c61
kelly-fu-ics4u1-02-25 <kelly.fu@student.tdsb.on.ca>
2025-02-26 17:55:20 +0000

    Update and finished halfly

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -6,4 +6,26 @@ public class PracticeProblem {
 
 	}
 
+	//1
+	public static void swap(ArrayList<String> list, int num1, int num2){ 
+		String exchange = list.get(num1);
+		list.set(num1,list.get(num2));
+		list.set(num2,exchange);
+	}
+
+	//2
+	public static ArrayList<Double> createArrayList(double[] arr){
+		ArrayList<Double> arrL = new ArrayList<>();
+		for (int i = 0; i < arr.length; i++){
+			arrL.add(arr[i]);
+			
+		}
+			return arrL;
+	}	
+
+	//3
+	public static 
+
+
+
 }
